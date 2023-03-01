5bcbb07e64530deafefcf5f18b93690148c0a050
x1_medusa_1x <aliniiova@yahoo.com>
2023-03-02 00:46:01 +0200

    Item Stack
    Managed to fix the stacking of items.
    Added a max amount of items.

--- Assets/SFX/Inventory/ItemIcons/Scripts/InventoryManager.cs
+++ Assets/SFX/Inventory/ItemIcons/Scripts/InventoryManager.cs
@@ -1,4 +1,3 @@
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,19 +6,49 @@ using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
-    public List<Item> Items = new List<Item>();
+    //public List<Item> Items = new List<Item>();
+    private Dictionary<Item, int> Items = new Dictionary<Item, int>();
+    private GameObject obj = null;
 
     public Transform ItemContent;
     public GameObject InventoryItem;
 
+    public Transform PreviewItemContent;
+    public GameObject DescriptionField;
+    public GameObject TitleField;
+    public GameObject ImageField;
+    public GameObject BottomSeparator;
+
     private void Awake()
     {
         Instance = this;
+        if (Items.Count == 0)
+        {
+            DescriptionField.SetActive(false);
+            TitleField.SetActive(false);
+            ImageField.SetActive(false);
+            BottomSeparator.SetActive(false);
+        }
     }
 
     public void Add(Item item)
     {
-        Items.Add(item);
+        if (Items.Count == 0)
+        {
+            DescriptionField.SetActive(true);
+            TitleField.SetActive(true);
+            ImageField.SetActive(true);
+            BottomSeparator.SetActive(true);
+
+        }
+        if (Items.TryGetValue(item, out var current))
+        {
+            Items[item] = ++current;
+        }
+        else
+        {
+            Items.Add(item, 0);
+        }
     }
 
     public void Remove(Item item)
@@ -27,20 +56,44 @@ public class InventoryManager : MonoBehaviour
         Items.Remove(item);
     }
 
+    private void ShowItem(Item item, int count)
+    {
+        if (count % item.maxAmount == 0)
+        {
+            obj = Instantiate(InventoryItem, ItemContent);
+            var itemIcon = obj.transform.Find("Icon").GetComponent<Image>();
+            var itemCount = obj.transform.Find("CountNumber").GetComponent<TMP_Text>();
+            var button = obj.transform.gameObject.GetComponent<Button>();
+
+            button.onClick.AddListener(delegate { PressButton(item); }); ;
+            itemCount.text = $"{count % item.maxAmount + 1}";
+            itemIcon.sprite = item.icon;
+        }
+        else
+        {
+            var itemCount = obj.transform.Find("CountNumber").GetComponent<TMP_Text>();
+            itemCount.text = $"{count % item.maxAmount + 1}";
+
+        }
+    }
+
     public void ListItems()
     {
         foreach (var item in Items)
         {
-            GameObject obj = Instantiate(InventoryItem, ItemContent);
-            GameObject objSprite = obj.transform.GetChild(0).gameObject;
-            var itemName = objSprite.transform.GetComponent<TextMeshProUGUI>();
-            var itemIcon = objSprite.transform.GetComponent<Image>();
-
-            Debug.Log(JsonConvert.SerializeObject(InventoryItem));
-
-            itemName.text = item.itemName;
-            itemIcon.sprite = item.icon;
+            ShowItem(item.Key, item.Value);
         }
+    }
+
+    public void PressButton(Item item)
+    {
+        var itemDescription = DescriptionField.transform.GetComponent<TMP_Text>();
+        var itemTitle = TitleField.transform.GetComponent<TMP_Text>();
+        var itemImage = ImageField.transform.GetComponent<Image>();
+
+        itemDescription.text = item.description;
+        itemTitle.text = item.title;
+        itemImage.sprite = item.icon;
     }
 
 }
--- Assets/SFX/Inventory/ItemIcons/Scripts/Item.cs
+++ Assets/SFX/Inventory/ItemIcons/Scripts/Item.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     public int id;
-    public string itemName;
-    public int value;
+    public string title;
+    public string description;
+    public int maxAmount;
     public Sprite icon;
 }
